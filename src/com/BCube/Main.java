8fafe50406c07a85db51611d8dafd324e6e3cecb
thamma <dominiczimmer@online.de>
2015-03-03 20:45:58 +0100

    Middle Move added

--- src/com/BCube/Main.java
+++ src/com/BCube/Main.java
@@ -5,8 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		Cube c = new Cube();
 		c.printCube();
-		//c.move(Turn.Up);
-		c.move(Turn.Left);
+		c.move(Turn.Up);
+		c.move(Turn.Up);
+		c.move(Turn.Middle);
+		c.move(Turn.Middle);
+		c.move(Turn.Up);
+		c.move(Turn.Up);
+		c.move(Turn.MiddlePrime);
+		c.move(Turn.MiddlePrime);
 		c.printCube();
 	}
 
--- src/com/BCube/Turn.java
+++ src/com/BCube/Turn.java
@@ -1,120 +1,66 @@
 package com.BCube;
 
 public enum Turn {
-	Up(new int[][] { new int[] { 0, 0 }, new int[] { 0, 1 },
-			new int[] { 0, 2 }, new int[] { 0, 3 }, new int[] { 0, 5 },
-			new int[] { 0, 6 }, new int[] { 0, 7 }, new int[] { 0, 8 },
-			new int[] { 2, 2 }, new int[] { 2, 5 }, new int[] { 2, 8 },
-			new int[] { 4, 6 }, new int[] { 4, 7 }, new int[] { 4, 8 },
-			new int[] { 5, 0 }, new int[] { 5, 3 }, new int[] { 5, 6 },
-			new int[] { 1, 0 }, new int[] { 1, 1 }, new int[] { 1, 2 }, }, new int[][] { new int[] { 0, 2 }, new int[] { 0, 5 },
-					new int[] { 0, 8 }, new int[] { 0, 1 },
-					new int[] { 0, 7 }, new int[] { 0, 0 },
-					new int[] { 0, 3 }, new int[] { 0, 6 },
-					new int[] { 4, 6 }, new int[] { 4, 7 },
-					new int[] { 4, 8 }, new int[] { 5, 0 },
-					new int[] { 5, 3 }, new int[] { 5, 6 },
-					new int[] { 1, 0 }, new int[] { 1, 1 },
-					new int[] { 1, 2 }, new int[] { 2, 2 },
-					new int[] { 2, 5 }, new int[] { 2, 8 }, })
-					,UpPrime(Turn.Up.getDest(), Turn.Up.getOrig()),Front(
-							new int[][] {
-									new int[]{1,0},
-									new int[]{1,1},
-									new int[]{1,2},
-									new int[]{1,3},
-									new int[]{1,5},
-									new int[]{1,6},
-									new int[]{1,7},
-									new int[]{1,8},
-									new int[]{3,6},
-									new int[]{3,7},
-									new int[]{3,8},
-									new int[]{2,6},
-									new int[]{2,7},
-									new int[]{2,8},
-									new int[]{0,6},
-									new int[]{0,7},
-									new int[]{0,8},
-									new int[]{5,6},
-									new int[]{5,7},
-									new int[]{5,8},
-							},
-							new int[][] {
-									new int[]{1,2},
-									new int[]{1,5},
-									new int[]{1,8},
-									new int[]{1,1},
-									new int[]{1,7},
-									new int[]{1,0},
-									new int[]{1,3},
-									new int[]{1,6},
-									new int[]{2,6},
-									new int[]{2,7},
-									new int[]{2,8},
-									new int[]{0,6},
-									new int[]{0,7},
-									new int[]{0,8},
-									new int[]{5,6},
-									new int[]{5,7},
-									new int[]{5,8},
-									new int[]{3,6},
-									new int[]{3,7},
-									new int[]{3,8},
-							}
-							
-							
-							), 
-					
-					FrontPrime(Turn.Front.getDest(), Turn.Front.getOrig()),
-					Left(     new int[][] {
-									new int[]{3,0},
-									new int[]{3,1},
-									new int[]{3,2},
-									new int[]{3,3},
-									new int[]{3,5},
-									new int[]{3,6},
-									new int[]{3,7},
-									new int[]{3,8},
-									new int[]{1,0},
-									new int[]{1,3},
-									new int[]{1,6},
-									new int[]{3,2},
-									new int[]{3,5},
-									new int[]{3,8},
-									new int[]{4,0},
-									new int[]{4,3},
-									new int[]{4,6},
-									new int[]{0,0},
-									new int[]{0,3},
-									new int[]{0,6},
-									
-							},
-							new int[][] {
-									new int[]{3,2},
-									new int[]{3,5},
-									new int[]{3,8},
-									new int[]{3,1},
-									new int[]{3,7},
-									new int[]{3,0},
-									new int[]{3,3},
-									new int[]{3,6},
-									new int[]{3,2},
-									new int[]{3,5},
-									new int[]{3,8},
-									new int[]{4,0},
-									new int[]{4,3},
-									new int[]{4,6},
-									new int[]{0,0},
-									new int[]{0,3},
-									new int[]{0,6},
-									new int[]{1,0},
-									new int[]{1,3},
-									new int[]{1,6},
-							}
-							), LeftPrime(Turn.Left.getDest(),Turn.Left.getOrig())
-							//,Down, Back, Right, Middle, Slice, Equator
-					;
+	Up(
+			new int[][] { new int[] { 0, 0 }, new int[] { 0, 1 },
+					new int[] { 0, 2 }, new int[] { 0, 3 }, new int[] { 0, 5 },
+					new int[] { 0, 6 }, new int[] { 0, 7 }, new int[] { 0, 8 },
+					new int[] { 2, 2 }, new int[] { 2, 5 }, new int[] { 2, 8 },
+					new int[] { 4, 6 }, new int[] { 4, 7 }, new int[] { 4, 8 },
+					new int[] { 5, 0 }, new int[] { 5, 3 }, new int[] { 5, 6 },
+					new int[] { 1, 0 }, new int[] { 1, 1 }, new int[] { 1, 2 }, },
+			new int[][] { new int[] { 0, 2 }, new int[] { 0, 5 },
+					new int[] { 0, 8 }, new int[] { 0, 1 }, new int[] { 0, 7 },
+					new int[] { 0, 0 }, new int[] { 0, 3 }, new int[] { 0, 6 },
+					new int[] { 4, 6 }, new int[] { 4, 7 }, new int[] { 4, 8 },
+					new int[] { 5, 0 }, new int[] { 5, 3 }, new int[] { 5, 6 },
+					new int[] { 1, 0 }, new int[] { 1, 1 }, new int[] { 1, 2 },
+					new int[] { 2, 2 }, new int[] { 2, 5 }, new int[] { 2, 8 }, }), UpPrime(
+			Turn.Up.getDest(), Turn.Up.getOrig()), Front(new int[][] {
+			new int[] { 1, 0 }, new int[] { 1, 1 }, new int[] { 1, 2 },
+			new int[] { 1, 3 }, new int[] { 1, 5 }, new int[] { 1, 6 },
+			new int[] { 1, 7 }, new int[] { 1, 8 }, new int[] { 3, 6 },
+			new int[] { 3, 7 }, new int[] { 3, 8 }, new int[] { 2, 6 },
+			new int[] { 2, 7 }, new int[] { 2, 8 }, new int[] { 0, 6 },
+			new int[] { 0, 7 }, new int[] { 0, 8 }, new int[] { 5, 6 },
+			new int[] { 5, 7 }, new int[] { 5, 8 }, }, new int[][] {
+			new int[] { 1, 2 }, new int[] { 1, 5 }, new int[] { 1, 8 },
+			new int[] { 1, 1 }, new int[] { 1, 7 }, new int[] { 1, 0 },
+			new int[] { 1, 3 }, new int[] { 1, 6 }, new int[] { 2, 6 },
+			new int[] { 2, 7 }, new int[] { 2, 8 }, new int[] { 0, 6 },
+			new int[] { 0, 7 }, new int[] { 0, 8 }, new int[] { 5, 6 },
+			new int[] { 5, 7 }, new int[] { 5, 8 }, new int[] { 3, 6 },
+			new int[] { 3, 7 }, new int[] { 3, 8 }, }), FrontPrime(Turn.Front
+			.getDest(), Turn.Front.getOrig()), Left(new int[][] {
+			new int[] { 3, 0 }, new int[] { 3, 1 }, new int[] { 3, 2 },
+			new int[] { 3, 3 }, new int[] { 3, 5 }, new int[] { 3, 6 },
+			new int[] { 3, 7 }, new int[] { 3, 8 }, new int[] { 1, 0 },
+			new int[] { 1, 3 }, new int[] { 1, 6 }, new int[] { 3, 2 },
+			new int[] { 3, 5 }, new int[] { 3, 8 }, new int[] { 4, 0 },
+			new int[] { 4, 3 }, new int[] { 4, 6 }, new int[] { 0, 0 },
+			new int[] { 0, 3 }, new int[] { 0, 6 },
+
+	}, new int[][] { new int[] { 3, 2 }, new int[] { 3, 5 },
+			new int[] { 3, 8 }, new int[] { 3, 1 }, new int[] { 3, 7 },
+			new int[] { 3, 0 }, new int[] { 3, 3 }, new int[] { 3, 6 },
+			new int[] { 3, 2 }, new int[] { 3, 5 }, new int[] { 3, 8 },
+			new int[] { 4, 0 }, new int[] { 4, 3 }, new int[] { 4, 6 },
+			new int[] { 0, 0 }, new int[] { 0, 3 }, new int[] { 0, 6 },
+			new int[] { 1, 0 }, new int[] { 1, 3 }, new int[] { 1, 6 }, }), LeftPrime(
+			Turn.Left.getDest(), Turn.Left.getOrig()),
+	// ,Down, Back, Right,
+	Middle(new int[][] { new int[] { 0, 1 }, new int[] { 0, 4 },
+			new int[] { 0, 7 }, new int[] { 1, 1 }, new int[] { 1, 4 },
+			new int[] { 1, 7 }, new int[] { 3, 7 }, new int[] { 3, 4 },
+			new int[] { 3, 1 }, new int[] { 4, 1 }, new int[] { 4, 4 },
+			new int[] { 4, 7 } }, new int[][] { new int[] { 4, 1 },
+			new int[] { 4, 4 }, new int[] { 4, 7 }, new int[] { 0, 1 },
+			new int[] { 0, 4 }, new int[] { 0, 7 }, new int[] { 1, 1 },
+			new int[] { 1, 4 }, new int[] { 1, 7 }, new int[] { 3, 7 },
+			new int[] { 3, 4 }, new int[] { 3, 1 } }), MiddlePrime(Turn.Middle
+			.getDest(), Turn.Middle.getOrig())
+	// Slice, Equator
+	;
 
 	private int[][] orig;
 	private int[][] dest;
@@ -123,11 +69,11 @@ public enum Turn {
 		this.orig = arg1;
 		this.dest = arg2;
 	}
-	
+
 	public int[][] getOrig() {
 		return this.orig;
 	}
-	
+
 	public int[][] getDest() {
 		return this.dest;
 	}
